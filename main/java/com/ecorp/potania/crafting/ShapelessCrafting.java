97d9c25a24ed32387f7d13c92846efd9944e22ab
Frederic Schwieren <fred.schwieren@web.de>
2016-05-04 18:14:22 +0200

    test commit

--- main/java/com/ecorp/potania/crafting/ShapelessCrafting.java
+++ main/java/com/ecorp/potania/crafting/ShapelessCrafting.java
@@ -8,14 +8,14 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class ShapelessCrafting {
-	
-	
-	public static void register (){
-		
-		
-		GameRegistry.addShapelessRecipe(new ItemStack(PotaniaItemsRegistry.bud_grinded,2), new ItemStack (PotaniaItemsRegistry.bud),new ItemStack(PotaniaItemsRegistry.grinder, 1));
-		
-	}
-	
-	
+
+
+    public static void register() {
+
+
+        GameRegistry.addShapelessRecipe(new ItemStack(PotaniaItemsRegistry.bud_grinded, 2), new ItemStack(PotaniaItemsRegistry.bud), new ItemStack(PotaniaItemsRegistry.grinder, 1));
+
+    }
+
+
 }
